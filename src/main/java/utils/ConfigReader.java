79f0481f05e35d45b0fe4276041458b1bb29ada0
burcuakkayaa <burcuakkayaiu@gmail.com>
2022-03-13 12:40:01 +0300

    initial commit

---
+++ src/main/java/utils/ConfigReader.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private Properties prop;
+
+
+    public Properties initProp() throws IOException {
+        prop = new Properties();
+        FileInputStream ip = new FileInputStream(".\\src\\test\\resources\\config.properties");
+        prop.load(ip);
+
+
+        return prop;
+
+    }
+}
---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constants {
+
+    public static final String baseURL = "https://petstore.swagger.io";
+
+    public static final String basePath ="/v2";
+
+    public static final String endpoint = "/pet";
+
+}
